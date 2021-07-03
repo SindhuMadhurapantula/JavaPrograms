3f7cd51e79545e41dafc64744ee7985b9b0c253d
SindhuMadhurapantula <71440058+SindhuMadhurapantula@users.noreply.github.com>
2021-07-03 07:30:02 +0530

    Add files via upload

---
+++ Inc_Dec_Num.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner s =new Scanner(System.in);
+		int N=s.nextInt();
+		if(N%4==0)
+		{
+			System.out.print(++N);
+		}
+		else
+		{
+			System.out.print(--N);
+		}
+	}
+}
