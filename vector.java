4403d3893285a69e5eb3ff2d7e50502708704b0c
SindhuMadhurapantula <71440058+SindhuMadhurapantula@users.noreply.github.com>
2021-07-18 11:42:47 +0530

    Add files via upload

---
+++ vector.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class vector
+{
+public static void main(String args[])
+{
+Vector v=new Vector();
+v.add(101);
+v.add("rama");
+v.add(6.58);
+v.add(102);
+v.add("gita");
+v.add(8.98);
+System.out.println(v);
+v.remove(4);
+System.out.println(v+" ");
+int k=v.indexOf(6.58);
+v.remove(k);
+v.add(k,9.17);
+System.out.println(v+" ");
+System.out.println(v.contains("sita"));
+}
+}
